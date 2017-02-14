f8a30ca6c23f174b45a2264b2c2af4449686b511
JoySeeDog <zhoujianquan@pwrd.com>
2017-02-14 12:05:27 +0800

    Initial Commit

---
+++ JSDDevelopDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSDDevelopDemo
+//
+//  Created by jianquan on 14/02/2017.
+//  Copyright © 2017 JoySeeDog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSDDevelopDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JSDDevelopDemo
+//
+//  Created by jianquan on 14/02/2017.
+//  Copyright © 2017 JoySeeDog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
